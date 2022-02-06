4be9690a0493738d2362bdcd97c7534d9f3fb6fc
2019T00389 <2019t00389@stu.cmb.ac.lk>
2022-02-07 02:03:32 +0530

    1st commit

--- src/com/carpark/car.java
+++ src/com/carpark/car.java
@@ -1,7 +1,3 @@
-package com.carpark;
-
-public class car {
-}
 public class Vehicle {
 
     private String model;
@@ -30,3 +26,38 @@ public class Vehicle {
     public String getColor(){
         return color;
     }
+
+    public int NumOfWheels(){
+        return numOfWheels;
+    }
+
+    public String transmission(){
+        return transmission;
+    }
+
+
+    public void setNumOfWheels (int numOfWheels){
+        this.numOfWheels = numOfWheels;
+    }
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public void setTransmission(String transmission){
+        this.transmission = transmission;
+    }
+
+
+    public void printDetails(){
+        System.out.println("The color of this vehicle is " + this.color);
+        System.out.println("The model of this vehicle is " + this.model);
+        System.out.println("The transmission for this vehicle is " + this.transmission);
+        System.out.println("The number of wheels in this vehicle is "+ this.numOfWheels);
+    }
+
+}
